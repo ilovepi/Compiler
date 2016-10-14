2b698066c90fcac517eff247009bb223af468410
Paul Kirth <pk1574@gmail.com>
2016-10-13 19:31:51 -0700

    Added a main function to test with, and finished basics of Token

---
+++ src/compiler/Main.java
@@ -0,0 +1,19 @@
+package compiler;
+
+import compiler.lexer.Token;
+
+/**
+ * Created by Paul Kirth on 10/13/16.
+ */
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Token c;
+        c = Token.classifyToken("<-");
+
+        System.out.println(Token.printToken(c));
+
+
+    }
+}
--- src/compiler/lexer/Token.java
+++ src/compiler/lexer/Token.java
@@ -29,7 +29,7 @@ public enum  Token {
     LET, CALL, IF, THEN, ELSE, FI,WHILE, DO, OD, RETURN,
     MAIN,
 
-    IDENTIFIER, NUMBER,
+    IDENTIFIER, NUMBER, FUNCTION, PROCEDURE,
 
     // parsing tokens
     EOF,
@@ -37,8 +37,16 @@ public enum  Token {
     UNKNOWN;
 
 
+    /**
+     * Classify Tokens based on the Grammar
+     * TODO: consider using a hash table instead of enum + function... could be easier to maintian ???
+     * @param str The token(string) to classify
+     * @return The correct token of the string
+     */
+
     public static Token classifyToken(String str)
     {
+        //
         switch (str)
         {
             case "+":
@@ -49,63 +57,191 @@ public enum  Token {
                 return TIMES;
             case "/":
                 return DIVIDE;
+
+            // Relational Operators
             case "==":
                 return EQUAL;
             case "!=":
                 return NOT_EQUAL;
             case "<":
                 return LESS;
-
             case "<=":
                 return LESS_EQ;
             case ">":
                 return GREATER;
-            case "<=":
+            case ">=":
                 return GREATER_EQ;
-            case "/<-":
+
+            // Misc operators
+            case "<-":
                 return ASSIGN;
             case ";":
                 return SEMI_COLON;
             case ",":
                 return COMMA;
+
+            // Blocks
             case "(":
                 return OPEN_PAREN;
-
             case ")":
                 return CLOSE_PAREN;
-            case "-":
-                return MINUS;
-            case "*":
-                return TIMES;
-            case "/":
-                return DIVIDE;
-            case "==":
-                return EQUAL;
-            case "!=":
-                return NOT_EQUAL;
-            case "<":
-                return LESS;
+            case "[":
+                return OPEN_BRACKET;
+            case "]":
+                return CLOSE_BRACKET;
+            case "{":
+                return OPEN_CURL;
+            case "}":
+                return CLOSE_CURL;
 
-            case "+":
-                return PLUS;
-            case "-":
-                return MINUS;
-            case "*":
-                return TIMES;
-            case "/":
-                return DIVIDE;
-            case "==":
-                return EQUAL;
-            case "!=":
-                return NOT_EQUAL;
-            case "<":
-                return LESS;
+            //keywords
+            case "let":
+                return LET;
+            case "call":
+                return CALL;
+
+            case "if":
+                return IF;
+            case "then":
+                return THEN;
+            case "else":
+                return ELSE;
+            case "fi":
+                return FI;
+            case "while":
+                return WHILE;
+            case "do":
+                return DO;
+            case "od":
+                return OD;
+
+            case "return":
+                return RETURN;
+
+            case "main":
+                return MAIN;
+            case "function":
+                return FUNCTION;
+            case "procedure":
+                return PROCEDURE;
+            case "@":
+                return EOF;
+
+            default:
+                if(str.matches("[0-9]+"))
+                {
+                    return NUMBER;
+                }
+                else if (str.matches("[a-z][a-z0-9]*"))
+                {
+                    return IDENTIFIER;
+                }
+                else
+                {
+                    return UNKNOWN;
+                }
         }
 
     }
 
+    /**
+     * A to String function for printing Token values
+     * @param t The token to read
+     * @return String representation of the token
+     */
+    public static String printToken(Token t)
+    {
+        //
+        switch (t)
+        {
+            case PLUS:
+                return "+";
+            case MINUS:
+                return "-";
+            case TIMES:
+                return "*";
+            case DIVIDE:
+                return "/";
 
+            // Relational Operators
+            case EQUAL:
+                return "==";
+            case NOT_EQUAL:
+                return "!=";
+            case LESS:
+                return "<";
+            case LESS_EQ:
+                return "<=";
+            case GREATER:
+                return ">";
+            case GREATER_EQ:
+                return ">=";
 
+            // Misc operators
+            case ASSIGN:
+                return "<-";
+            case SEMI_COLON:
+                return ";";
+            case COMMA:
+                return ",";
+
+            // Blocks
+            case OPEN_PAREN:
+                return "(";
+            case CLOSE_PAREN:
+                return ")";
+            case OPEN_BRACKET:
+                return "[";
+            case CLOSE_BRACKET:
+                return "]";
+            case OPEN_CURL:
+                return "{";
+            case CLOSE_CURL:
+                return "}";
+
+            //keywords
+            case LET:
+                return "let";
+            case CALL:
+                return "call";
+
+            case IF:
+                return "if";
+            case THEN:
+                return "then";
+            case ELSE:
+                return "else";
+            case FI:
+                return "fi";
+            case WHILE:
+                return "while";
+            case DO:
+                return "do";
+            case OD:
+                return "od";
+
+            case RETURN:
+                return "return";
+
+            case MAIN:
+                return "main";
+            case FUNCTION:
+                return "function";
+            case PROCEDURE:
+                return "procedure";
+            case EOF:
+                return "EOF";
+            case NUMBER:
+                return "number";
+            case IDENTIFIER:
+                return "identifier";
+            case UNKNOWN:
+                return "unknown";
+            default:
+                return "ERROR";
+        }
+
+    }
 
 
 }
