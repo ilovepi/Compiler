53440b01e1feaaba17ce83d3661d305f629e252b
Paul Kirth <pk1574@gmail.com>
2016-12-18 19:13:02 -0800

    update the parser from other branch

--- src/compiler/parser/ParseException.java
+++ src/compiler/parser/ParseException.java
@@ -3,19 +3,28 @@ package compiler.parser;
 /**
  * Created by paul on 12/18/16.
  */
-public class ParserException extends Exception{
-    private int lineNumber;
-    private int offset;
+public class ParserException extends Exception {
+    public ParserException(String msg) {
+        super(msg);
+    }
 
-    ParserException(String expected, String found, int line)
-    {
-        super("");
-        String msg = "Parse Error at line " + line +
-                " : Expected to find '" + expected +
-                "', found '" +found +"' instead.";
-        super.
+    public ParserException(TokenNode currToken, Token expected) {
+        super("Parser Exception: Syntax Error at line " + currToken.getLineNumber() +
+                ": Expected to find '" + Token.toString(expected) +
+                "', instead found '" + currToken.getS() + "'.");
+    }
 
-        line
+
+    public ParserException(TokenNode currToken, Token expected, String filename) {
+        super("Parser Exception: Syntax Error at line " + currToken.getLineNumber() +
+                " of file '" + filename + "': Expected to find '" + Token.toString(expected) +
+                "', instead found '" + currToken.getS() + "'.");
+    }
+
+    public ParserException(TokenNode currToken, Token expected, String filename, String function) {
+        super("Parser Exception: Syntax Error in function '" + function + "' at line " + currToken.getLineNumber() +
+                " of file '" + filename + "': Expected to find '" + Token.toString(expected) +
+                "', instead found '" + currToken.getS() + "'.");
     }
 
 }
