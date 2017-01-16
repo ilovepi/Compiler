2cb0c57516ef0bc02a3afe89e6d6a3e37b4d3b2e
Paul Kirth <pk1574@gmail.com>
2017-01-15 23:14:28 -0800

    improve unit test for exception and reading the contents of the test file

--- compiler/frontend/test/LexerTest.cs
+++ compiler/frontend/test/LexerTest.cs
@@ -20,16 +20,20 @@ namespace compiler.frontend.test
         {
             lex = new Lexer(TestContext.CurrentContext.TestDirectory + @"\frontend\test\LexerTest1.txt");            
 
-            string str = "Read some character";
+            // Exact string contents of LExerTest1.txt
+            string str = "Read some characters";
 
             foreach(char c in str)
             {
                 char b = lex.next();
-                Console.WriteLine("Scanner expects '" + c + "', but read '" + b +"'");
-                Assert.AreEqual( c, b);
+                //Console.WriteLine("Scanner expects '" + c + "', but read '" + b +"'");
+                Assert.AreEqual(c, b);
             }
 
-            //Assert.Throws( )
+            // make sure we throw an exception for reading past the end of the file
+            var ex = Assert.Throws<Exception>(() => lex.next());
+            Assert.That(ex.Message, Is.EqualTo("Error: Lexer cannot read beyond the end of the file"));
+
         }
     }
 }
