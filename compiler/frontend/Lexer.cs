ef5d38100365ee989ca203ed323da850b160f64c
Paul Kirth <pk1574@gmail.com>
2017-01-15 21:47:18 -0800

    reintroduce classes

---
+++ compiler/frontend/Lexer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace compiler.frontend
+{
+    class Lexer
+    {
+        StreamReader sr;
+        char c;
+
+        Lexer(string filename)
+        {
+            sr = new StreamReader(filename);
+
+            next();
+
+        }
+
+        ~Lexer()
+        {
+            if(sr != null)
+            {
+                sr.Close();
+                sr = null;
+            }
+        }
+
+        public void next()
+        {
+            if(sr.Peek() == -1)
+            {
+                throw new Exception("Error: Lexer cannot read beyond the end of the file");
+            }
+            c = (char)sr.Read();
+        }
+    }
+}
---
+++ compiler/frontend/LexerTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compiler.frontend
+{
+    [TestFixture]
+    public class LexerTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
