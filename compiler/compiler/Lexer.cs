1ea7e3c856a221293e1e6496089d7af3bdac0f0a
Paul Kirth <pk1574@gmail.com>
2017-01-15 21:29:07 -0800

    Restructure Project for C#

---
+++ compiler/compiler/Lexer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace compiler
+{
+    public class Lexer
+    {
+        public char input;
+        StreamReader fr;
+
+        public Lexer(string filename)
+        {            
+            fr = new StreamReader(filename);
+
+        }
+
+
+    }
+}
