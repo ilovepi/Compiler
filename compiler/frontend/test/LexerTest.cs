77635bf32b0ef899d833a3a94b8220423724051b
Paul Kirth <pk1574@gmail.com>
2017-01-17 00:29:43 -0800

    udate project settings to help unit tests

--- compiler/frontend/test/LexerTest.cs
+++ compiler/frontend/test/LexerTest.cs
@@ -12,12 +12,12 @@ namespace compiler.frontend.test
         //[DeploymentItem("LexerTest1.txt", "targetFolder")]
         public void nextTest()
         {
-            lex = new Lexer(TestContext.CurrentContext.TestDirectory + @"/frontend/test/LexerTest1.txt");            
+            lex = new Lexer(TestContext.CurrentContext.TestDirectory + @"/frontend/test/LexerTest1.txt");
 
             // Exact string contents of LExerTest1.txt without the '.'
             string str = "Read some characters";
 
-            foreach(char c in str)
+            foreach (char c in str)
             {
                 //Console.WriteLine("Scanner expects '" + c + "', but read '" + b +"'");
                 Assert.AreEqual(c, lex.c);
@@ -32,7 +32,43 @@ namespace compiler.frontend.test
         [Test]
         public void getNextTokenTest()
         {
-            Assert.Fail();
+            lex = new Lexer(TestContext.CurrentContext.TestDirectory + @"/frontend/test/testdata/test001.txt");
+            Token[] expected_toks = new Token[]{Token.COMMENT,
+                                                    Token.MAIN,
+                                                    Token.VAR,
+                                                    Token.IDENTIFIER,
+                                                    Token.COMMA,
+                                                    Token.IDENTIFIER,
+                                                    Token.SEMI_COLON,
+                                                    Token.OPEN_CURL,
+                                                    Token.LET,
+                                                    Token.IDENTIFIER,
+                                                    Token.ASSIGN,
+                                                    Token.NUMBER,
+                                                    Token.SEMI_COLON,
+                                                    Token.LET,
+                                                    Token.IDENTIFIER,
+                                                    Token.ASSIGN,
+                                                    Token.NUMBER,
+                                                    Token.TIMES,
+                                                    Token.IDENTIFIER,
+                                                    Token.SEMI_COLON,
+                                                    Token.CALL,
+                                                    Token.IDENTIFIER,
+                                                    Token.OPEN_PAREN,
+                                                    Token.IDENTIFIER,
+                                                    Token.CLOSE_PAREN,
+                                                    Token.CLOSE_CURL,
+                                                    Token.EOF
+                                                    };
+
+            Token t;
+
+            foreach (Token parsed_token in expected_toks)
+            {
+                t = lex.getNextToken();
+                Assert.AreEqual(t, parsed_token);
+            }    
         }
 
     }
