d02447faf39dcc1816e4e1f46f1ef050cb51aa28
czaer <faraz.zaerpoor@gmail.com>
2017-01-20 14:59:08 -0800

    Designator

--- compiler/frontend/Parser.cs
+++ compiler/frontend/Parser.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace compiler.frontend
 {
+    public Token t;
+    public Lexer s;
+
     class Parser
     {
+        public void next() {
+            t = s.getNextToken();
+        }
+
+        public void Designator(){
+            if (t == Token.IDENTIFIER) {
+                next();
+            }
+            else error();
+        }
+        public void Factor(){
+            
+        }
+        public void Term(){
+            
+        }
+        public void Expression(){
+            
+        }
+        public void Relation(){
+            
+        }
+
     }
 }
