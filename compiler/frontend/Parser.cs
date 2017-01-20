5f510329f51c1a082c887c47c6e61713c94c4596
Paul Kirth <pk1574@gmail.com>
2017-01-20 15:59:06 -0800

    Faraz is dumb

--- compiler/frontend/Parser.cs
+++ compiler/frontend/Parser.cs
@@ -3,26 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using compiler.frontend;
 
 namespace compiler.frontend
 {
-    public Token t;
-    public Lexer s;
 
-    class Parser
-    {
+	class Parser
+	{
+		public Token t;
+		public Lexer s;
+
+		public void getExpected(Token expected)
+		{
+			if (t == expected)
+			{
+				next();
+			}
+			error();s
+		}
+
         public void next() {
             t = s.getNextToken();
         }
 
-        public void Designator(){
-            if (t == Token.IDENTIFIER) {
+        public void Designator() {
+            if (t == Token.IDENTIFIER)
+			{
                 next();
             }
             else error();
+
+			if (t == Token.OPEN_BRACKET)
+			{
+				next();
+			}
+			else error();
+
+			while (t != Token.CLOSE_BRACKET)
+			{
+				Expression();
+				next();
+			}
+
         }
         public void Factor(){
-            
+			if ((t == Token.IDENTIFIER) || (t == Token.IDENTIFIER))
+			{
+				next();
+			}
+			else error();
         }
         public void Term(){
             
