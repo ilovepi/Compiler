40d75a89801cc0945d7ac3eea34e524fb97e3b74
Paul Kirth <pk1574@gmail.com>
2017-01-28 08:27:58 -0800

    Add BasicBlocks and Instructions

--- compiler/middlend/ir/BasicBlock.cs
+++ compiler/middlend/ir/BasicBlock.cs
@@ -8,12 +8,29 @@ namespace compiler.middleend.ir
 
 		public List<Instruction> Instructions{ get; set; }
 
+		public Anchor Anchors { get; set; }
+
+
 
 		public BasicBlock()
 		{
 			Instructions = new List<Instruction>();
+
+			Anchors = new Anchor();
 		}
 
+
+		public void AddInstruction(Instruction ins)
+		{
+			
+		}
+
+
+		public Instruction search(Instruction ins)
+		{
+
+			return Anchor.
+		}
 
 
 
--- compiler/middlend/ir/Instruction.cs
+++ compiler/middlend/ir/Instruction.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 namespace compiler.middleend.ir
 {
-	public class Instruction
+	public class Instruction : IEquatable<Instruction>
 	{
-		public Instruction()
+		public int Num { get; set; }
+		public int Op { get; set; }
+		public int Arg1 { get; set; }
+		public int Arg2 { get; set; }
+
+
+		public Instruction(int pNum, int pOp, int pArg1, int pArg2)
 		{
+			Num = pNum;
+			Op = pOp;
+			Arg1 = pArg1;
+			Arg2 = pArg2;
+		}
+
+		public bool Equals(Instruction other)
+		{
+			//TODO: determine if the instruction number is important in comparison
+			return (Op == other.Op) && (Arg1 == other.Arg1) && (Arg2 == other.Arg2);
 		}
 	}
 }
