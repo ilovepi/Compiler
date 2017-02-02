41d1c8a8c36f3bf8ad4fac9437164066959a621d
Paul Kirth <pk1574@gmail.com>
2017-02-02 14:11:47 -0800

    Remove dumb instruction class and improve IR

--- compiler/middlend/ir/Instruction.cs
+++ compiler/middlend/ir/Instruction.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using compiler.middlend.ir;
+
 namespace compiler.middleend.ir
 {
 	public class Instruction : IEquatable<Instruction>
@@ -19,13 +21,13 @@ namespace compiler.middleend.ir
 		/// The First Argument in the Instruction
 		/// </summary>
 		/// <value>The arg1.</value>
-		//public int Arg1 { get; set; }
+		public Operand Arg1 { get; set; }
 
 		/// <summary>
 		/// The Second Argument in the instruction
 		/// </summary>
 		/// <value>The arg2.</value>
-		//public int Arg2 { get; set; }
+		public Operand Arg2 { get; set; }
 
 		/// <summary>
 		/// Linked list pointer to the previous instruction
@@ -42,27 +44,53 @@ namespace compiler.middleend.ir
 		/// </summary>
 		public Instruction Search { set; get; }
 
-		public Instruction(int pNum, int pOp, int pArg1, int pArg2)
+		public Instruction(int pNum, int pOp, Operand pArg1, Operand pArg2)
 		{
 			Num = pNum;
 			Op = pOp;
-			//Arg1 = pArg1;
-			//Arg2 = pArg2;
+			Arg1 = pArg1;
+			Arg2 = pArg2;
 
 			Prev = null;
 			Next = null;
 			Search = null;
 		}
 
-		public bool Equals(Instruction other)
-		{
-			//TODO: determine if the instruction number is important in comparison
-		    return (Op == other.Op);  // && (Arg1 == other.Arg1) && (Arg2 == other.Arg2);
-		}
 
+	    public bool Equals(Instruction other)
+	    {
+	        if (ReferenceEquals(null, other)) return false;
+	        if (ReferenceEquals(this, other)) return true;
+	        return Op == other.Op && Equals(Arg1, other.Arg1) && Equals(Arg2, other.Arg2);
+	    }
 
+	    public override bool Equals(object obj)
+	    {
+	        if (ReferenceEquals(null, obj)) return false;
+	        if (ReferenceEquals(this, obj)) return true;
+	        if (obj.GetType() != this.GetType()) return false;
+	        return Equals((Instruction) obj);
+	    }
 
+	    public override int GetHashCode()
+	    {
+	        unchecked
+	        {
+	            var hashCode = Op;
+	            hashCode = (hashCode * 397) ^ (Arg1 != null ? Arg1.GetHashCode() : 0);
+	            hashCode = (hashCode * 397) ^ (Arg2 != null ? Arg2.GetHashCode() : 0);
+	            return hashCode;
+	        }
+	    }
 
+	    public static bool operator ==(Instruction left, Instruction right)
+	    {
+	        return Equals(left, right);
+	    }
 
+	    public static bool operator !=(Instruction left, Instruction right)
+	    {
+	        return !Equals(left, right);
+	    }
 	}
 }
--- compiler/middlend/ir/InstructionAI.cs
+++
@@ -1,9 +0,0 @@
-﻿using compiler.middleend.ir;
-
-namespace compiler.middlend.ir
-{
-    public class InstructionAI : Instruction
-    {
-
-    }
-}
