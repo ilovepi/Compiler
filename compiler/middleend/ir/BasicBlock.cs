71806e50bfd55617b0e0dec6c210ccf27b06e59e
Paul Kirth <pk1574@gmail.com>
2017-02-07 15:25:28 -0800

    Reformatted code, and adressed potential issue with dominator node insertions

--- compiler/middleend/ir/BasicBlock.cs
+++ compiler/middleend/ir/BasicBlock.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 
 namespace compiler.middleend.ir
 {
-	//TODO: redisign this class and its supporting classes
-	public class BasicBlock
-	{
-        public string Name { get;  }
-		public List<Instruction> Instructions{ get; set; }
+    //TODO: redisign this class and its supporting classes
+    public class BasicBlock
+    {
+        public string Name { get; }
+        public List<Instruction> Instructions { get; set; }
 
-		public Anchor AnchorBlock { get; set; }
+        public Anchor AnchorBlock { get; set; }
 
 
-
-		public BasicBlock()
-		{
-			Instructions = new List<Instruction>();
-			AnchorBlock = new Anchor();
-		}
+        public BasicBlock()
+        {
+            Instructions = new List<Instruction>();
+            AnchorBlock = new Anchor();
+        }
 
         public BasicBlock(string pName)
         {
@@ -27,34 +26,27 @@ namespace compiler.middleend.ir
         }
 
 
-
         public void AddInstruction(Instruction ins)
-		{
-			Instructions.Add(ins);
-			AnchorBlock.Insert(ins);
-		}
+        {
+            Instructions.Add(ins);
+            AnchorBlock.Insert(ins);
+        }
 
 
-		public Instruction Search(Instruction ins)
-		{
-			var instList = AnchorBlock.FindOpChain(ins.Op);
+        public Instruction Search(Instruction ins)
+        {
+            var instList = AnchorBlock.FindOpChain(ins.Op);
 
-		    if (instList != null)
-		    {
+            if (instList != null)
+            {
+                foreach (var item in instList)
+                {
+                    if (item.Equals(ins))
+                        return item;
+                }
+            }
 
-		        foreach (var item in instList)
-		        {
-		            if (item.Equals(ins))
-		                return item;
-		        }
-		    }
-
-		    return null;
-		}
-
-
-
-
-
-	}
+            return null;
+        }
+    }
 }
--- compiler/middleend/ir/DominatorNode.cs
+++ compiler/middleend/ir/DominatorNode.cs
@@ -29,13 +29,30 @@ namespace compiler
 
         public virtual void InsertChild(DominatorNode other)
         {
+            // detatch other's subtree from any tree it previously belonged to
+            if (!other.IsRoot())
+            {
+                other.Parent.RemoveChild(other);
+            }
+
+            // add other to this subtree
             Children.Add(other);
+
+            other.Parent = this;
         }
         
 
+        //TODO: Do we need this method?
         virtual public void UpdateParent(DominatorNode other)
         {
             Parent = other;
         }
+
+
+        public bool RemoveChild(DominatorNode other)
+        {
+            return Children.Remove(other);
+        }
+
     }
 }
