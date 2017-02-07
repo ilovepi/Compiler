aa115608188d765e23be30c8e3da84ec70994af9
czaer <faraz.zaerpoor@gmail.com>
2017-02-07 15:41:56 -0800

    Added DomTree with DFS traversal

---
+++ compiler/middleend/ir/DomTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace compiler
+{
+    class DomTree
+    {
+        public DominatorNode Root { get; set; }
+        
+
+        public DomTree()
+        {
+            Root = null;
+        }
+
+
+        public void RecursiveDFS(DominatorNode curNode)
+        {
+            // TODO: include display or print function here
+            foreach (DominatorNode child in curNode.Children)
+            {
+                if (child != null)
+                {
+                    RecursiveDFS(child);
+                }
+            }
+        }
+
+        public void DFSTraversal()
+        {
+            if (Root != null)
+            {
+                RecursiveDFS(Root);
+            }
+        }
+
+    }
+}
