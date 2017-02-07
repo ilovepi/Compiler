4c7bd20d258eef29098857a87df7c659ae8de79d
czaer <faraz.zaerpoor@gmail.com>
2017-02-07 13:41:04 -0800

    Added DominatorNode class

---
+++ compiler/middleend/ir/DominatorNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using compiler.middleend.ir;
+
+namespace compiler
+{
+    class DominatorNode : Node
+    {
+        public DominatorNode(BasicBlock pBB) : base(pBB)
+        {
+            Parent = null;
+            Child = null;
+        }
+    }
+}
