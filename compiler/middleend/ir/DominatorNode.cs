25065c4869784423f74010b77f4ffd6b088b068e
Paul Kirth <pk1574@gmail.com>
2017-02-07 15:32:09 -0800

    Added documentation to Dominator node

--- compiler/middleend/ir/DominatorNode.cs
+++ compiler/middleend/ir/DominatorNode.cs
@@ -6,13 +6,26 @@ namespace compiler
 {
     class DominatorNode
     {
+        /// <summary>
+        /// The basic block of the node
+        /// </summary>
         public BasicBlock BB { get; set; }
 
+        /// <summary>
+        /// The parent of this node
+        /// </summary>
 		public DominatorNode Parent { get; set; }
 
+        /// <summary>
+        /// The Nodes which this basic block directly dominates
+        /// </summary>
         public List<DominatorNode> Children;
 
 
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="pBB">The basic block of the node</param>
         public DominatorNode(BasicBlock pBB)
         {
             BB = pBB;
@@ -21,12 +34,20 @@ namespace compiler
         }
 
 
+        /// <summary>
+        /// Checks if this node is a root node
+        /// </summary>
+        /// <returns>True if this node is a root, with no parents</returns>
         public bool IsRoot()
         {
             return (Parent == null);
         }
 
 
+        /// <summary>
+        /// Adds a new subtree to this node.
+        /// </summary>
+        /// <param name="other"></param>
         public virtual void InsertChild(DominatorNode other)
         {
             // detatch other's subtree from any tree it previously belonged to
@@ -37,7 +58,6 @@ namespace compiler
 
             // add other to this subtree
             Children.Add(other);
-
             other.Parent = this;
         }
         
@@ -49,6 +69,11 @@ namespace compiler
         }
 
 
+        /// <summary>
+        /// Removes a node from this subtree
+        /// </summary>
+        /// <param name="other"> the root of the subtree to remove</param>
+        /// <returns>True if a node was removed, false if the node was not found</returns>
         public bool RemoveChild(DominatorNode other)
         {
             return Children.Remove(other);
