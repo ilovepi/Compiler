5c2628db64769abd78ff135bd2d9140a5beeefcb
Paul Kirth <pk1574@gmail.com>
2017-02-07 21:49:43 -0800

    Update Main to print the graph

--- compiler/Program/Program.cs
+++ compiler/Program/Program.cs
@@ -8,20 +8,34 @@ namespace Program
         //TODO: adjust main to use the parser when it is complete
         static void Main(string[] args)
         {
-            using (Lexer l = new Lexer(@"../../testdata/big.txt"))
+//            using (Lexer l = new Lexer(@"../../testdata/big.txt"))
+//            {
+//                Token t;
+//                do
+//                {
+//                    t = l.GetNextToken();
+//                    Console.WriteLine(TokenHelper.PrintToken(t));
+//
+//                } while (t != Token.EOF);
+//
+//                // necessary when testing on windows with visual studio
+//                //Console.WriteLine("Press 'enter' to exit ....");
+//                //Console.ReadLine();
+//            }
+            
+            using (Parser p = new Parser(@"../../testdata/test002.txt"))
             {
-                Token t;
-                do
+                p.Parse();
+                p.FlowCfg.GenerateDOTOutput();
+
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter("graph.txt"))
                 {
-                    t = l.GetNextToken();
-                    Console.WriteLine(TokenHelper.PrintToken(t));
+                    file.WriteLine( p.FlowCfg.DOTOutput);
+                }
 
-                } while (t != Token.EOF);
-
-                // necessary when testing on windows with visual studio
-                //Console.WriteLine("Press 'enter' to exit ....");
-                //Console.ReadLine();
             }
+            
+            
         }
     }
 }
