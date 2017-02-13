ce60186784848751cb5ad07c32ea8e33fda4cd00
Paul Kirth <pk1574@gmail.com>
2017-02-13 00:26:35 -0800

    Remove dead code

--- compiler/Program/Program.cs
+++ compiler/Program/Program.cs
@@ -8,20 +8,6 @@ namespace Program
         //TODO: adjust main to use the parser when it is complete
         private static void Main(string[] args)
         {
-//            using (Lexer l = new Lexer(@"../../testdata/big.txt"))
-//            {
-//                Token t;
-//                do
-//                {
-//                    t = l.GetNextToken();
-//                    Console.WriteLine(TokenHelper.PrintToken(t));
-//
-//                } while (t != Token.EOF);
-//
-//                // necessary when testing on windows with visual studio
-//                //Console.WriteLine("Press 'enter' to exit ....");
-//                //Console.ReadLine();
-//            }
 
             using (var p = new Parser(@"../../testdata/test003.txt"))
             {
