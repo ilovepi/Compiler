4025d77328b1ad17dc2ae72a680c85a76f7ac83e
Paul Kirth <pk1574@gmail.com>
2017-02-16 22:19:55 -0800

    Add ParseResult to support dynamic ssa values

---
+++ compiler/middleend/ir/ParseResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compiler.middleend.ir
+{
+    class ParseResult
+    {
+        public Operand Operand { get; set; }
+        public List<Instruction> Instructions { get; set; }
+
+        public Dictionary<SsaVariable, Instruction> VarTable { get; set; }
+
+        public ParseResult()
+        {
+            Operand = null;
+            Instructions = null;
+            VarTable = new Dictionary<SsaVariable, Instruction>();
+        }
+
+        public ParseResult(Dictionary<SsaVariable, Instruction> symTble )
+        {
+            Operand = null;
+            Instructions = null;
+            VarTable = new Dictionary<SsaVariable, Instruction>(symTble);
+        }
+
+        public ParseResult(Operand pOperand, List<Instruction> pInstructions, Dictionary<SsaVariable, Instruction> pSymTble)
+        {
+            Operand = pOperand;
+            Instructions = new List<Instruction>(pInstructions);
+            VarTable = new Dictionary<SsaVariable, Instruction>(pSymTble);
+        }
+
+
+    }
+}
