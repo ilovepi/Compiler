a47c02deb2bd50fa91e8d3d03e4e0ce0b03ac58f
czaer <faraz.zaerpoor@gmail.com>
2017-03-08 13:23:17 -0800

    Implemented instruction coloring/spilled for InterferenceGraph. Doesn't do anything yet.

--- compiler/middleend/ir/InterferenceGraph.cs
+++ compiler/middleend/ir/InterferenceGraph.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using QuickGraph;
@@ -12,6 +13,7 @@ using System.Linq.Expressions;
 using NUnit.Framework.Internal.Commands;
 using compiler.midleend.ir;
 using System.Runtime.InteropServices;
+using NUnit.Framework;
 using QuickGraph.Graphviz;
 using QuickGraph.Graphviz.Dot;
 
@@ -23,9 +25,24 @@ namespace compiler.middleend.ir
 	{
 		public InterferenceGraph() { }
 
-		public InterferenceGraph(BasicBlock block)
+        // # of available registers
+	    private const int REGISTER_COUNT = 28;
+
+        // Generic copy of this graph (for mutation), built alongside Interference Graph
+        private UndirectedGraph<Instruction, Edge<Instruction>> copy = new UndirectedGraph<Instruction, Edge<Instruction>>();
+
+        // Colored and spilled instructions
+        public Dictionary<Instruction, int> GraphColors = new Dictionary<Instruction, int>();
+        public List<Instruction> spilledInstr = new List<Instruction>();
+
+        public InterferenceGraph(BasicBlock block)
 		{
 			AddVertexRange(block.Instructions);
+            copy.AddVertexRange(block.Instructions);
+
+            AddInterferenceEdges(block);
+
+		    Color();
 		}
 
 		public void AddInterferenceEdges(BasicBlock block)
@@ -37,23 +54,90 @@ namespace compiler.middleend.ir
 				    if (item != null)
 				    {
 				        AddVerticesAndEdge(new Edge<Instruction>(instruction, item));
+				        copy.AddVerticesAndEdge(new Edge<Instruction>(instruction, item));
 				    }
 				}
 			}
 		}
 
-		public void Color()
+        private Stack<Instruction> coloringStack = new Stack<Instruction>();
+
+        private void ColorRecursive(UndirectedGraph<Instruction, Edge<Instruction>> curGraph)
 		{
-			// if we have enough registers, allocate all results to a register
-			if (VertexCount < 28)
-			{ 
-			
+            // We have to spill if we don't find a vertex with low enough edges.
+		    bool spill = true;
+
+			// Base case (empty graph)
+			if (curGraph.VertexCount == 0)
+			{
+			    return;
 			}
 
-			// otherwise color each register
-			var highest = this.Vertices.OrderByDescending(item => AdjacentDegree(item)).First();
+            // Step through vertices by descending edge count
+		    foreach (var vertex in curGraph.Vertices.OrderByDescending(item => AdjacentDegree(item)))
+		    {
+                // Pick a node with fewer neighbors than the max
+		        if (AdjacentDegree(vertex) < REGISTER_COUNT)
+		        {
+                    // Put that node on the coloring stack and remove it from graph
+                    coloringStack.Push(vertex);
+                    curGraph.RemoveVertex(vertex);
+		            spill = false;
+		            continue;
+		        }
+		    }
 
+            // If we don't find an appropriate vertex, we'll have to spill.
+		    if (spill)
+		    {
+                // By default, spills the instruction with the most dependencies
+                // TODO: Maybe come up with a better spilling heuristic
+		        var highest = curGraph.Vertices.OrderByDescending(item => AdjacentDegree(item)).First();
+                spilledInstr.Add(highest);
+                curGraph.RemoveVertex(highest);
+            }
+
+            // Either way, we've removed a vertex and logged it. Time for the subgraph.
+            ColorRecursive(curGraph);
 		}
 
-    }
+	    private void Color()
+	    {
+            Stack<Instruction> coloringStack = new Stack<Instruction>();
+            List<Instruction> spilledInstr = new List<Instruction>();
+
+            // Call recursive coloring fxn with the mutable copy
+            ColorRecursive(copy);
+
+            // Until there are no more instructions to be colored...
+	        while (coloringStack.Count != 0)
+	        {
+	            // ... pop an instruction from the stack...
+	            Instruction curInstr = coloringStack.Pop();
+
+                // ... get a list of its neighbors' already assigned registers...
+                List<int> neighborRegs = new List<int>();
+	            foreach (Instruction neighbor in curInstr.LiveRange)
+	            {
+	                if (GraphColors.ContainsKey(neighbor))
+	                {
+	                    neighborRegs.Add(GraphColors[neighbor]);
+	                }
+	            }
+
+                // ... and give it a different one.
+	            for (int reg = 1; reg <= REGISTER_COUNT; reg++)
+	            {
+	                if (!neighborRegs.Contains(reg))
+	                {
+	                    GraphColors.Add(curInstr, reg);
+	                }
+	            }
+
+                // All coloring stack values should be assigned a color
+	            Debug.Assert(GraphColors.ContainsKey(curInstr), "Did not color colorable reg.");
+	        }
+	    }
+
+	}
 }
