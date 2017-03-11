a36e5d8c2be19147f5be118a5791e623b9018a3b
Paul Kirth <pk1574@gmail.com>
2017-03-10 20:35:18 -0800

    Add Code Generation

---
+++ compiler/backend/DlxInstruction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using compiler.middleend.ir;
+
+namespace compiler.backend
+{
+    class DlxInstruction
+    {
+        public DlxInstruction(Instruction inst)
+        {
+            
+        }
+
+        opc
+    }
+}
