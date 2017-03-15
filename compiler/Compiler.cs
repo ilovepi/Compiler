d0d5e1af0373f64092e98cf2ccd8ff928e3e4156
Paul Kirth <pk1574@gmail.com>
2017-03-14 18:49:47 -0700

    Turn optimizations back on

--- compiler/Compiler.cs
+++ compiler/Compiler.cs
@@ -314,8 +314,8 @@ namespace compiler
                 GraphOutput = true,
                 CopyProp = true,
                 Cse = true,
-                DeadCode = false,
-                PruneCfg = false,
+                DeadCode = true,
+                PruneCfg = true,
                 RegAlloc = true,
                 InstSched = false,
                 CodeGen = false
