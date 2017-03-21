4462750a1e0f12ed445f8b0fee28298a34bdf46f
Paul Kirth <pk1574@gmail.com>
2017-03-21 06:49:21 -0700

    Readd simulator file

---
+++ simulator/Simulator/src/dlx/DLXsim.java
@@ -0,0 +1,37 @@
+package dlx;
+
+public class DLXsim {
+    public static void main(String[] args) {
+        try {
+            String input = null;
+            boolean outTime = false;
+            for(int i = 0; i<args.length; i++) {
+                switch(args[i]) {
+                    case "-time":
+                        outTime = true;
+                        break;
+                    default:
+                        // default is intput file name, will only execute
+                        // a single file, so last one read will win.
+                        input = args[i];
+                        break;
+                }
+            }
+            if(input == null) {
+                System.out.println("No input file provided");
+                System.exit(1);
+            }
+
+            int[] code = DLXutil.readCode(input);
+            DLX.load(code);
+            int time = DLX.execute();
+            if(outTime) {
+                System.err.print(time);
+            }
+            System.exit(0);
+        } catch (Exception e) {
+            System.out.println("An error occured");
+            System.exit(-1);
+        }
+    }
+}
